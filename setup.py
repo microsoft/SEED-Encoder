e1b5a16748b53d6a999f89d7889680ca28cf942a
playing-code <lsqrucer@gmail.com>
2021-09-23 02:44:41 +0000

    update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
+    name='ANCE',
+    version='0.1.0',
+    description='Less is More: Pre-train a Strong Text Encoder for Dense Retrieval Using a Weak Decoder',
+    url='https://github.com/microsoft/SEED-Encoder',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+   license="MIT",
+   long_description=readme,
    install_requires=[
         'transformers==2.3.0', 
         'pytrec-eval',
